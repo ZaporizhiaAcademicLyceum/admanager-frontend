ed6fc6ae3b7d867b4aca77e5611e2ac5281a41f2
Alexander Drozdov <snosme@gmail.com>
2019-08-31 13:31:36 +0300

    debug backend

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: '/',
   devServer: {
-    host: '192.168.1.101'
+    host: '192.168.1.101',
+    proxy: {
+      '/api': {
+        target: 'https://192.168.1.150:5001',
+        changeOrigin: true
+      }
+    }
   }
 }
